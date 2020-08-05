radians to degrees fix

ovationpyme/ovation_utilities.py
@@ -231,8 +231,8 @@ def brekke_moen_solar_conductance(dt,glats,glons,f107):
         f107, float
             F10.7 index (daily) used to calcuate solar conductance
     """
-    szas_rad = sun.solar_zenith_angle(dt, glats, glons)
-    # szas_rad = szas/180.*np.pi
+    szas = np.rad2deg(sun.solar_zenith_angle(dt, glats, glons))
+    szas_rad = szas/180.*np.pi
 
     sigp,sigh = np.zeros_like(glats),np.zeros_like(glats)
 